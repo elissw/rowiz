1da4def5b13c4f0f4fcffc209d6bbd2797ec8c2d
elissw <elisso.stam@gmail.com>
2026-05-19 10:30:37 +0200

    Added lollipop geometry

--- R/viz_geometry_wrappers.R
+++ R/viz_geometry_wrappers.R
@@ -84,3 +84,81 @@ geom_histogram_rowiz <- function(mapping = NULL,
     ...
   )
 }
+
+
+#' Custom ggplot style geometry to plot a lollipop plot
+#' Purpose: To avoid manually adding points and segments etc
+#' Use: Exactly like any other ggplot geom. Just use the x and y you want your points to be at
+#' Defaults will be overriden if specified otherwise in the arguments. 
+#' @name geom_lollipop_rowiz
+#' @export
+
+# The new ggplot geometry (no need to export)
+GeomLollipop <- ggplot2::ggproto(
+  "GeomLollipop", ggplot2::Geom,
+
+  required_aes = c("x", "y", "xend", "yend"),
+
+  default_aes = ggplot2::aes(
+    colour = "cadetblue",
+    alpha = 1,
+    linewidth = 0.8,
+    size = 3
+  ),
+
+draw_panel = function(data, panel_params, coord) {
+
+  data <- coord$transform(data, panel_params)
+
+  segment_data <- data
+  segment_data$xend <- data$xend
+  segment_data$yend <- data$yend
+
+  segments <- grid::segmentsGrob(
+    x0 = segment_data$x,
+    y0 = segment_data$y,
+    x1 = segment_data$xend,
+    y1 = segment_data$yend,
+    gp = grid::gpar(
+      col = data$colour,
+      lwd = data$linewidth *4
+    )
+  )
+
+  points <- grid::pointsGrob(
+    x = data$xend,
+    y = data$yend,
+    pch = 16,
+    gp = grid::gpar(
+      col = scales::alpha(data$colour, data$alpha),
+      fontsize = data$size *4
+    )
+  )
+
+  grid::grobTree(segments, points)
+}
+)
+
+geom_lollipop_rowiz <- function(mapping = NULL,
+                          data = NULL,
+                          stat = "identity",
+                          position = "identity",
+                          ...,
+                          na.rm = FALSE,
+                          show.legend = NA,
+                          inherit.aes = TRUE) {
+
+  ggplot2::layer(
+    geom = GeomLollipop,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    inherit.aes = inherit.aes,
+    show.legend = show.legend,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
